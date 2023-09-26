4fe7d1863313909056777db6b3576520f0ac16f8
LittlerXu <2281482223@qq.com>
2023-09-26 22:42:58 +0800

    remove bat

--- bin/autoDeploy.bat
+++
@@ -1,8 +0,0 @@
-call yarn docs:build
-cd docs/.vitepress/dist
-
-git init
-git add -A
-git commit -m "auto construct blog"
-
-git push -f https://github.com/LittlerXu/LittlerXu.github.io.git master:gh-pages
