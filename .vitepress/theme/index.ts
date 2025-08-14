0c5bfb27913c82b27266ae848ff0c05bcbad31a1
徐佳豪 <xujiahao5@xiaomi.com>
2025-08-14 23:04:20 +0800

    feat: 引入位置更正

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import Advertising from './components/advertising.vue'
+import Advertising from './components/Advertising.vue'
 import './style.css'
 
 export default {
